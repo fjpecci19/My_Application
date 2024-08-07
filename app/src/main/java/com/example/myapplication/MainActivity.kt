56121feee3e4983c2626ff36d5189e1b4eb74310
Franco Pecci <frankdev04@gmail.com>
2024-08-07 19:56:00 -0400

    Columnas scrolleables

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,45 +3,87 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Columnas()
         }
     }
 }
 
+@Preview(showBackground = false)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Columnas() {
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(align = Alignment.Center)
+        .padding(5.dp)
+        .verticalScroll(rememberScrollState())) {
+        Texto(texto = "Jean")
+        Espacio()
+        Texto(texto = "Cabañas")
+        Texto(texto = "Quintana")
+        Texto(texto = "Dejesús")
+        Texto(texto = "León")
+        Espacio()
+        Texto(texto = "Iturbe")
+        Texto(texto = "Piris Da Motta")
+        Texto(texto = "Viera")
+        Texto(texto = "Domínguez")
+        Espacio()
+        Texto(texto = "Da Costa")
+        Texto(texto = "Fernández")
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun Filas() {
+    Row {
+        Texto(texto = "Jean")
+        Texto(texto = "Cabañas")
+    }
+}
+
+@Composable
+fun Texto(texto: String) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = texto,
+        color = Color.White,
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 50.sp,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF1C2833))
+            .padding(5.dp)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
+fun Espacio() {
+    Spacer(modifier = Modifier.height(5.dp))
 }
